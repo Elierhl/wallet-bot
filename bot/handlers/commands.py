c2584872d02a8c9344ee1b36e56d9992b3d51408
Denis Bulychev <denis_bulychev@epam.com>
2023-07-07 00:13:49 +0300

    Test pre-commit.

--- bot/handlers/commands.py
+++ bot/handlers/commands.py
@@ -1,5 +1,5 @@
 from aiogram import Router, html
-from aiogram.filters import CommandStart, Command
+from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -14,6 +14,4 @@ async def cmd_start(message: Message, session: AsyncSession):
     query = select(User.name).where(User.user_id == 1)
     res = await session.execute(query)
     res = res.scalar()
-    await message.answer(
-        f"This is res!!! = {res}"
-    )
+    await message.answer(f"This is res = {res}")
