4ad2ceeeba353488df2e13d3b94afd6a5b3c1a39
anthony <antonwowis@yandex.ru>
2023-07-16 23:29:49 +0300

    fix alembic

--- bot/db/schemas.py
+++ bot/db/schemas.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Double, ForeignKey, String, func
-from sqlalchemy.dialects.mysql import DATETIME, TINYINT
+from sqlalchemy import ForeignKey, String, func
+from sqlalchemy.dialects.mysql import DATETIME, DOUBLE
 from sqlalchemy.orm import Mapped, as_declarative, mapped_column, relationship
 
 
@@ -9,12 +9,12 @@ class Base:
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
 
 
-class BaseWithDates(Base):
+class Dates:
     created_at = mapped_column(DATETIME, default=func.now())
     updated_at = mapped_column(DATETIME, onupdate=func.now())
 
 
-class Users(BaseWithDates):
+class Users(Base, Dates):
     __tablename__ = "users"
 
     tg_id: Mapped[int] = mapped_column(unique=True, comment="Telegram id")
@@ -26,7 +26,7 @@ class Users(BaseWithDates):
     wallet = relationship("Wallets", back_populates="user")
 
 
-class UserSettings(BaseWithDates):
+class UserSettings(Base, Dates):
     __tablename__ = "user_settings"
 
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
@@ -36,12 +36,13 @@ class UserSettings(BaseWithDates):
     user = relationship("Users", foreign_keys=[user_id])
 
 
-class Wallets(BaseWithDates):
+class Wallets(Base, Dates):
     __tablename__ = "wallets"
 
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     currency_id: Mapped[int] = mapped_column(ForeignKey("currency.id"))
-    amount: Mapped[Double]
+    amount = mapped_column(DOUBLE, nullable=False)
+    freeze_amount = mapped_column(DOUBLE, nullable=False)
     address: Mapped[str] = mapped_column(String(50))
 
     user = relationship("Users", back_populates="wallet")
@@ -53,47 +54,46 @@ class Currency(Base):
     name: Mapped[str] = mapped_column(String(50))
 
 
-class Withdraw(BaseWithDates):
+class Withdraw(Base, Dates):
     __tablename__ = "withdraw"
 
-    amount: Mapped[Double]
+    amount = mapped_column(DOUBLE, nullable=False)
     currency_id: Mapped[int] = mapped_column(ForeignKey("currency.id"))
     wallet_id: Mapped[int] = mapped_column(ForeignKey("wallets.id"))
     address_to: Mapped[str] = mapped_column(String(50))
     aml: Mapped[str] = mapped_column(String(10))
     txid: Mapped[str] = mapped_column(String(100), comment="hash")
-    state: Mapped[TINYINT]
+    state: Mapped[bool]
     gate: Mapped[str] = mapped_column(String(50), index=True)
     refund: Mapped[bool]
-    # ??? Not sure what these fields are about ???
-    # chat_id: Mapped[Double]
-    # message_id: Mapped[Double]
-    # chat_name: Mapped[str] = mapped_column(String(100))
+    chat_id: Mapped[float]
+    message_id: Mapped[float]
+    chat_name: Mapped[str] = mapped_column(String(100))
 
     currency = relationship("Currency", foreign_keys=[currency_id])
     wallet = relationship("Wallets", foreign_keys=[wallet_id])
 
 
-class Deposit(BaseWithDates):
+class Deposit(Base, Dates):
     __tablename__ = "deposit"
 
-    amount: Mapped[Double]
+    amount = mapped_column(DOUBLE, nullable=False)
     currency_id: Mapped[int] = mapped_column(ForeignKey("currency.id"))
     wallet_id: Mapped[int] = mapped_column(ForeignKey("wallets.id"))
     address_from: Mapped[str] = mapped_column(String(50))
     aml: Mapped[str] = mapped_column(String(10))
     txid: Mapped[str] = mapped_column(String(100), comment="hash")
-    state: Mapped[TINYINT]
+    state: Mapped[bool]
     sign: Mapped[str] = mapped_column(String(50))
 
     currency = relationship("Currency", foreign_keys=[currency_id])
     wallet = relationship("Wallets", foreign_keys=[wallet_id])
 
 
-class Commission(BaseWithDates):
+class Commission(Base, Dates):
     __tablename__ = "commission"
 
-    amount: Mapped[Double]
+    amount = mapped_column(DOUBLE, nullable=False)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     currency_id: Mapped[int] = mapped_column(ForeignKey("currency.id"))
     active: Mapped[bool] = mapped_column(default=True)
@@ -102,26 +102,21 @@ class Commission(BaseWithDates):
     currency = relationship("Currency", foreign_keys=[currency_id])
 
 
-class Exchange(BaseWithDates):
+class Exchange(Base, Dates):
     __tablename__ = "exchange"
 
-    amount: Mapped[Double]
-    rate: Mapped[Double]
+    amount = mapped_column(DOUBLE, nullable=False)
+    rate = mapped_column(DOUBLE, nullable=False)
     wallet_id_from: Mapped[int] = mapped_column(ForeignKey("wallets.id"))
     wallet_id_to: Mapped[int] = mapped_column(ForeignKey("wallets.id"))
 
     wallet = relationship("Wallets", foreign_keys=[wallet_id_from, wallet_id_to])
 
 
-class Comments(BaseWithDates):
+class Comments(Base, Dates):
     __tablename__ = "comments"
 
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     comment: Mapped[str] = mapped_column(String(250))
 
     user = relationship("Users", foreign_keys=[user_id])
-
-
-# ??? Not sure what this table is about ???
-class Transactions(BaseWithDates):
-    __tablename__ = "transactions"
